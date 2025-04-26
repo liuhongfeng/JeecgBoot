cf36743d227b509cf47607bd356a5c5abd04577f
liuhongfeng <liuhongfeng239@163.com>
2025-04-26 18:41:14 +0800

    add: 福羲项目，vue 管理端新增「员工管理」功能菜单

---
+++ jeecgboot-vue3/src/views/staff/FcFishStaff.data.ts
@@ -0,0 +1,36 @@
+import {BasicColumn} from '/@/components/Table';
+import {FormSchema} from '/@/components/Table';
+import { rules} from '/@/utils/helper/validator';
+import { render } from '/@/utils/common/renderUtils';
+import { getWeekMonthQuarterYear } from '/@/utils';
+//列表数据
+export const columns: BasicColumn[] = [
+  {
+    title: '小程序用户',
+    align: "center",
+    dataIndex: 'realname'
+  },
+  {
+    title: '员工姓名',
+    align: "center",
+    dataIndex: 'name'
+  },
+  {
+    title: '员工手机号',
+    align: "center",
+    dataIndex: 'phone'
+  },
+  {
+    title: '钓场名称',
+    align: "center",
+    dataIndex: 'groundName'
+  },
+];
+
+// 高级查询数据
+export const superQuerySchema = {
+  realname: {title: '小程序用户',order: 0,view: 'popup', type: 'string',code: 'fc_fish_user_report', orgFields: 'realname', destFields: 'realname', popupMulti: false,},
+  name: {title: '员工姓名',order: 1,view: 'text', type: 'string',},
+  phone: {title: '员工手机号',order: 2,view: 'text', type: 'string',},
+  groundName: {title: '钓场名称',order: 3,view: 'popup', type: 'string',code: 'fc_fish_ground_report', orgFields: 'name', destFields: 'groundName', popupMulti: false,},
+};
---
+++ jeecgboot-vue3/src/views/staff/components/FcFishStaffForm.vue
@@ -0,0 +1,194 @@
+<template>
+  <a-spin :spinning="confirmLoading">
+    <JFormContainer :disabled="disabled">
+      <template #detail>
+        <a-form ref="formRef" class="antd-modal-form" :labelCol="labelCol" :wrapperCol="wrapperCol" name="FcFishStaffForm">
+          <a-row>
+						<a-col :span="24">
+							<a-form-item label="小程序用户" v-bind="validateInfos.realname" id="FcFishStaffForm-realname" name="realname">
+								<j-popup
+									placeholder="请选择小程序用户"
+									v-model:value="formData.realname"
+									code="fc_fish_user_report"
+									:fieldConfig="[
+										{ source: 'id', target: 'userId' },
+										{ source: 'realname', target: 'realname' },
+									]"
+									:multi="false"
+									:setFieldsValue="setFieldsValue"
+									 allow-clear />							</a-form-item>
+						</a-col>
+						<a-col :span="24">
+							<a-form-item label="员工姓名" v-bind="validateInfos.name" id="FcFishStaffForm-name" name="name">
+								<a-input v-model:value="formData.name" placeholder="请输入员工姓名"  allow-clear ></a-input>
+							</a-form-item>
+						</a-col>
+						<a-col :span="24">
+							<a-form-item label="员工手机号" v-bind="validateInfos.phone" id="FcFishStaffForm-phone" name="phone">
+								<a-input v-model:value="formData.phone" placeholder="请输入员工手机号"  allow-clear ></a-input>
+							</a-form-item>
+						</a-col>
+						<a-col :span="24">
+							<a-form-item label="钓场名称" v-bind="validateInfos.groundName" id="FcFishStaffForm-groundName" name="groundName">
+								<j-popup
+									placeholder="请选择钓场名称"
+									v-model:value="formData.groundName"
+									code="fc_fish_ground_report"
+									:fieldConfig="[
+										{ source: 'id', target: 'groundId' },
+										{ source: 'name', target: 'groundName' },
+									]"
+									:multi="false"
+									:setFieldsValue="setFieldsValue"
+									 allow-clear />							</a-form-item>
+						</a-col>
+          </a-row>
+        </a-form>
+      </template>
+    </JFormContainer>
+  </a-spin>
+</template>
+
+<script lang="ts" setup>
+  import { ref, reactive, defineExpose, nextTick, defineProps, computed, onMounted } from 'vue';
+  import { defHttp } from '/@/utils/http/axios';
+  import { useMessage } from '/@/hooks/web/useMessage';
+  import JPopup from '/@/components/Form/src/jeecg/components/JPopup.vue';
+  import { getValueType } from '/@/utils';
+  import { saveOrUpdate } from '../FcFishStaff.api';
+  import { Form } from 'ant-design-vue';
+  import JFormContainer from '/@/components/Form/src/container/JFormContainer.vue';
+  const props = defineProps({
+    formDisabled: { type: Boolean, default: false },
+    formData: { type: Object, default: () => ({})},
+    formBpm: { type: Boolean, default: true }
+  });
+  const formRef = ref();
+  const useForm = Form.useForm;
+  const emit = defineEmits(['register', 'ok']);
+  const formData = reactive<Record<string, any>>({
+    id: '',
+    realname: '',   
+    name: '',   
+    phone: '',   
+    groundName: '',   
+  });
+  const { createMessage } = useMessage();
+  const labelCol = ref<any>({ xs: { span: 24 }, sm: { span: 5 } });
+  const wrapperCol = ref<any>({ xs: { span: 24 }, sm: { span: 16 } });
+  const confirmLoading = ref<boolean>(false);
+  //表单验证
+  const validatorRules = reactive({
+    realname: [{ required: true, message: '请输入小程序用户!'},],
+    name: [{ required: true, message: '请输入员工姓名!'},],
+    phone: [{ required: true, message: '请输入员工手机号!'},],
+    groundName: [{ required: true, message: '请输入钓场名称!'},],
+  });
+  const { resetFields, validate, validateInfos } = useForm(formData, validatorRules, { immediate: false });
+
+  // 表单禁用
+  const disabled = computed(()=>{
+    if(props.formBpm === true){
+      if(props.formData.disabled === false){
+        return false;
+      }else{
+        return true;
+      }
+    }
+    return props.formDisabled;
+  });
+
+  
+  /**
+   * 新增
+   */
+  function add() {
+    edit({});
+  }
+
+  /**
+   * 编辑
+   */
+  function edit(record) {
+    nextTick(() => {
+      resetFields();
+      const tmpData = {};
+      Object.keys(formData).forEach((key) => {
+        if(record.hasOwnProperty(key)){
+          tmpData[key] = record[key]
+        }
+      })
+      //赋值
+      Object.assign(formData, tmpData);
+    });
+  }
+
+  /**
+   * 提交数据
+   */
+  async function submitForm() {
+    try {
+      // 触发表单验证
+      await validate();
+    } catch ({ errorFields }) {
+      if (errorFields) {
+        const firstField = errorFields[0];
+        if (firstField) {
+          formRef.value.scrollToField(firstField.name, { behavior: 'smooth', block: 'center' });
+        }
+      }
+      return Promise.reject(errorFields);
+    }
+    confirmLoading.value = true;
+    const isUpdate = ref<boolean>(false);
+    //时间格式化
+    let model = formData;
+    if (model.id) {
+      isUpdate.value = true;
+    }
+    //循环数据
+    for (let data in model) {
+      //如果该数据是数组并且是字符串类型
+      if (model[data] instanceof Array) {
+        let valueType = getValueType(formRef.value.getProps, data);
+        //如果是字符串类型的需要变成以逗号分割的字符串
+        if (valueType === 'string') {
+          model[data] = model[data].join(',');
+        }
+      }
+    }
+    await saveOrUpdate(model, isUpdate.value)
+      .then((res) => {
+        if (res.success) {
+          createMessage.success(res.message);
+          emit('ok');
+        } else {
+          createMessage.warning(res.message);
+        }
+      })
+      .finally(() => {
+        confirmLoading.value = false;
+      });
+  }
+
+  /**
+   *  popup组件值改变事件
+   */
+  function setFieldsValue(map) {
+    Object.keys(map).map((key) => {
+      formData[key] = map[key];
+    });
+  }
+
+  defineExpose({
+    add,
+    edit,
+    submitForm,
+  });
+</script>
+
+<style lang="less" scoped>
+  .antd-modal-form {
+    padding: 14px;
+  }
+</style>
