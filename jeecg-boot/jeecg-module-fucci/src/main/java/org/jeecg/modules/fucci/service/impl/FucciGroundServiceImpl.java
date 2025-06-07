ae86bc6754e82756e1ed339e3c92aec791a18e79
liuhongfeng <liuhongfeng239@163.com>
2025-06-07 21:10:19 +0800

    update: 修改预约订单，移除预约日期前三天内不可修改的校验

--- jeecg-boot/jeecg-module-fucci/src/main/java/org/jeecg/modules/fucci/service/impl/FucciGroundServiceImpl.java
+++ jeecg-boot/jeecg-module-fucci/src/main/java/org/jeecg/modules/fucci/service/impl/FucciGroundServiceImpl.java
@@ -1,6 +1,5 @@
 package org.jeecg.modules.fucci.service.impl;
 
-import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import com.alibaba.fastjson.JSONObject;
@@ -43,7 +42,10 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -307,11 +309,6 @@ public class FucciGroundServiceImpl implements IFucciGroundService {
         // 检查预约订单是否未修改过，修改次数 modifyCount 为 0 时，才可以修改
         if (0 != fcFishOrder.getModifyCount()) {
             throw new JeecgBootException("预约订单已修改过，无法再次修改");
-        }
-        // 检查预约订单日期是否可以修改
-        long betweenDay = DateUtil.between(fcFishOrder.getDate(), new Date(), DateUnit.HOUR);
-        if (new Date().after(fcFishOrder.getDate()) || betweenDay <= 72) {
-            throw new JeecgBootException("预约订单日期只能提前 3 天修改，当前时间无法修改");
         }
     }
 
